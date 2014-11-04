8237f96323dc9d80672e5e68abd20f1b1b1ed2bb
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2014-11-04 17:35:02 +0530

    - Made Fileupload REST endpoint work

---
+++ src/main/groovy/StormSpikeWSApplication.groovy
@@ -0,0 +1,14 @@
+import org.glassfish.jersey.media.multipart.MultiPartFeature
+import org.glassfish.jersey.server.ResourceConfig
+
+import javax.ws.rs.ApplicationPath
+
+@ApplicationPath("ws")
+public class StormSpikeWSApplication extends ResourceConfig {
+    StormSpikeWSApplication() {
+//        packages("org.foo.rest;org.bar.rest")
+        packages('ws')
+        register(MultiPartFeature.class)
+    }
+
+}
---
+++ src/main/groovy/ws/FileResource.groovy
@@ -0,0 +1,46 @@
+package ws
+
+import groovy.util.logging.Slf4j
+
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.HttpHeaders
+import javax.ws.rs.core.MediaType
+import org.glassfish.jersey.media.multipart.FormDataParam
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition
+
+import javax.ws.rs.core.Response
+
+@Path("/file")
+@Slf4j
+class FileResource {
+
+    @Context
+    private HttpHeaders headers
+
+    @GET
+    @Path("/ping")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String ping() {
+        "{greet: 'hello from file resource'}"
+    }
+
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+        sendToQueue(uploadedInputStream.text)
+        String response = "Pushed to Queue... $fileDetail.fileName"
+        return Response.status(200).entity(response).build()
+    }
+
+    private void sendToQueue(String message) {
+        log.info("Sending to Queue...$message")
+    }
+}
